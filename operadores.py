cd1231ec7cee8b810f69693c5f77818fd56d0ce4
CamiloAOsorno333 <bumeran333@gmai.com>
2026-03-23 17:13:48 -0500

    nuestro primer comentario en este curso

---
+++ operadores.py
@@ -0,0 +1,30 @@
+"""
+Operadores
+En una condición deben disponerse únicamente variables, valores constantes y operadores relacionales.
+
+_____________________________________________________________________________________________________
+
+Operadores Relacionales:
+== Igualdad
+
+!= Desigualdad
+
+< menor
+
+<= menor o igual
+
+> mayor
+
+>= mayor o igual
+__________________________________________________________________________________________________
+
+Operadores Matemáticos
++ suma
+- resta
+* multiplicación
+/ división de flotantes
+// división de enteros
+% resto de una división
+** exponenciación
+
+"""
